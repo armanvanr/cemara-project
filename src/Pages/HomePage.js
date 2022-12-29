38d4e1ede78565d5522dbbe0501cf25ac6d36674
oadyse <rainmaker8621@gmail.com>
2022-12-29 22:18:23 +0700

    add homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,77 @@
 import React from "react";
+import './HomePage.css';
+import logo from '../Assets/logo.jpg';
+import logos from '../Assets/logos.jpg';
 
 const HomePage = () =>{
     return(
-        <div>
-            Zoa Care Home Page
+        <div className="App">
+            {/* <div className="shape1"></div>
+            <div className="shape2"></div>
+            <div className="shape3"></div>
+            <div className="shape4"></div>
+            <div className="shape5"></div> */}
+
+            <div className="AppContainer">
+                <div className="AppNav">
+                    <img src="{logo}" className="Applogo"/>
+                    <div className="AppNavButton">
+                        <div className="AppNavItem">
+                            <span className="AppNavFont" style={{ width:'118px', height:'23px' }}>Daftar Donasi</span>
+                            <span className="AppNavFont" style={{ width:'72px', height:'23px' }}>Laporan</span>
+                            <span className="AppNavFont" style={{ width:'121px', height:'23px' }}>Tentang Kami</span>
+                            <span className="AppNavFont" style={{ width:'37px', height:'23px' }}>Aksi</span>
+                            <span className="AppNavFont" style={{ width:'56px', height:'23px' }}>Artikel</span>
+                        </div>
+                        <div className="LoginButton">
+                            <span className="LoginText">Login</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="AppHome">
+                    <div className="LeftSide">
+                        <div className="Menu" style={{ alignItems:'flex-end', order:'0' }}>
+                            <div className="MenuGroup">
+                                <img src="{logo}" className="MenuPic"/>
+                                <div className="MenuIsi" style={{ background:'#E92F2F' }}>
+                                    <span className="judul">Lapor!</span>
+                                    <span className="ket">lorem ipsum</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="Menu" style={{ alignItems:'center', order:'1' }}>
+                            <div className="MenuGroup">
+                                <img src="{logo}" className="MenuPic"/>
+                                <div className="MenuIsi" style={{ background:'#2E9C33' }}>
+                                    <span className="judul">Donasi</span>
+                                    <span className="ket">lorem ipsum</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="Menu" style={{ alignItems:'flex-start', order:'2' }}>
+                            <div className="MenuGroup">
+                                <img src="{logo}" className="MenuPic"/>
+                                <div className="MenuIsi" style={{ background:'#DCB736' }}>
+                                    <span className="judul">Adopsi</span>
+                                    <span className="ket">lorem ipsum</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="RightSide">
+                        <div className="up">
+                            <img src="{logos}"/>
+                            <div className="upTitle">We Care We Share</div>
+                            <div className="upIsi">
+                                Ciptakan hari esok yang lebih baik untuk satwa liar yang rentan dan peliharaan terlantar di sekitarmu. Jadilah pahlawan bagi bumi kita yang indah.
+                            </div>
+                        </div>
+                        <div className="bottom">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
