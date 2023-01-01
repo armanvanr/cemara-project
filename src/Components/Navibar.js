3db3c4b382d9ea4554715618a455c6e77e707b91
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-01-01 17:54:30 +0700

    Link for Routes

--- src/Components/Navibar.js
+++ src/Components/Navibar.js
@@ -1,21 +1,26 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import brand from '../Assets/Images/brand.jpg'
 import './Navibar.css';
 
 const Navibar = () => {
     return (
         <div className="navbar">
-            <img src={brand} className="navbar-brand" alt="" />
+            <NavLink to="/">
+                <img src={brand} className="navbar-brand" alt="" />
+            </NavLink>
+
             <div className="navbar-buttons">
                 <div className="navbar-items">
-                    <a href="/donationList">Daftar Donasi</a>
-                    <a href="/reportList">Laporan</a>
-                    <a href="/aboutUs">Tentang Kami</a>
-                    <a href="/action">Aksi</a>
-                    <a href="/articles">Artikel</a>
+
+                    <Link to="/donationList">Daftar Donasi</Link>
+                    <Link to="/reportList">Laporan</Link>
+                    <Link to="/aboutUs">Tentang Kami</Link>
+                    <Link to="/action">Aksi</Link>
+                    <Link to="/articles">Artikel</Link>
                 </div>
                 <button className="login-button">
-                    <a href="/login">Login</a>
+                    <Link to="/login">Login</Link>
                 </button>
             </div>
         </div>
