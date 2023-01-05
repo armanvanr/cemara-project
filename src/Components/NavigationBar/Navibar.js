de548082da9c6ac9a4e01b82c92c27ead8a11646
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-01-06 02:07:03 +0700

    Fixing navbar

---
+++ src/Components/NavigationBar/Navibar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import brand from '../../Assets/Images/brand.jpg'
+import './Navibar.css';
+
+const Navibar = () => {
+    return (
+        <div className="navbar">
+            <NavLink to="/">
+                <img src={brand} className="navbar-brand" alt="" />
+            </NavLink>
+
+            <div className="navbar-buttons">
+                <div className="navbar-items">
+                    <span to="/donationList">Daftar Donasi</span>
+                    <span to="/reportList">Laporan</span>
+                    <span to="/aboutUs">Tentang Kami</span>
+                    <span to="/action">Aksi</span>
+                    <span to="/articles">Artikel</span>
+                </div>
+                <button className="login-button">
+                    <Link to="/login">Login</Link>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Navibar;
