60058b94187ce855766f3523f87d4a6d456ad009
oadyse <rainmaker8621@gmail.com>
2023-01-12 08:52:12 +0700

    update the login page

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -34,7 +34,7 @@ const LoginPage = () =>{
 
     
     return(
-        <div className="App1">
+        <div className="AppForLogin">
             <div className="AppLogin">
                 <div className="leftSide">
                     <div className="loginContainer">
@@ -61,17 +61,31 @@ const LoginPage = () =>{
                                     </div>
                                         <div className="formBottom">
                                             <div className="button1" onClick={handleRegister}>
-                                                <span className="buttonText1">Don't have account?</span>
+                                                <span className="buttonText1">Buat Akun</span>
                                             </div>
                                             <div className="button2" onClick={handleLogin}>
-                                                <span className="buttonText2">Log In</span>
+                                                <span className="buttonText2">Masuk</span>
                                             </div>
                                         </div>
                                 </div>
                             </div>
                             <div className="footer-form">
-                                <div className="footerButton" style={{ width: '145px', order: '0' }}>Indonesia</div>
-                                <div className="footerButton" style={{ width: '136px', order: '1' }}>Help</div>
+                                <div className="footerButton" style={{ width: '145px', order: '0' }}>
+                                    Indonesia
+                                    <div className="footerButton-icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div className="footerButton" style={{ width: '136px', order: '1' }}>
+                                    <div className="footerButton-icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                    Bantuan
+                                </div>
                             </div>
                         </div>
                     </div>
