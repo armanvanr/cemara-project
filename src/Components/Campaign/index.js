33a7e714a947046b55a80fe5e832605ec647416f
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-01-15 17:13:59 +0700

    fixing className, clipRule, fillRule

--- src/Components/Campaign/index.js
+++ src/Components/Campaign/index.js
@@ -9,8 +9,8 @@ const Report = () => {
             <div className="campaign-button">
                 <div className="container-button">
                     <div className="container-button-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />
                         </svg>
                     </div>
                     <span className="container-button-text">Buat Baru</span>
@@ -24,7 +24,7 @@ const Report = () => {
                             <div className="campaign-card-up-title">
                                 <div className="campaign-card-up-title-text">Donasi pakan harimau Kebun Binatang Lato-Lato</div>
                                 <div className="campaign-card-up-title-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                     </svg>
                                 </div>
@@ -42,7 +42,7 @@ const Report = () => {
                             <div className="campaign-card-up-title">
                                 <div className="campaign-card-up-title-text">Donasi pakan harimau Kebun Binatang Lato-Lato</div>
                                 <div className="campaign-card-up-title-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                     </svg>
                                 </div>
